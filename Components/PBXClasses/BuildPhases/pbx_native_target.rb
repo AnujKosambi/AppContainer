5841a567b6f151b4eeb110965ac4bc555533e48d
Anuj Kosambi <anujkosambi@gmail.com>
2015-03-22 05:35:39 +0530

    Fixing Class Constancts

--- Components/PBXClasses/BuildPhases/pbx_native_target.rb
+++ Components/PBXClasses/BuildPhases/pbx_native_target.rb
@@ -1,25 +1,22 @@
 require_relative '../../abstract_object'
 module AppContainer
   class PBXNativeTarget < AbstractObject
-    attr_accessor :buildConfigurationList
-    attr_accessor :buildPhases
-    attr_accessor :buildRules
-    attr_accessor :dependencies
-    attr_accessor :name
-    attr_accessor :productName
-    attr_accessor :productReference
-    attr_accessor :productType
+    Fields =
+        ['buildConfigurationList',
+          'buildPhases',
+          'buildRules',
+          'dependencies',
+          'name',
+          'productName',
+          'productReference',
+          'productType']
 
     def initialize(hash={})
       super(self)
-      @buildConfigurationList = hash['buildConfigurationList']
-      @buildPhases = hash['buildPhases']
-      @buildRules = hash['buildRules']
-      @dependencies = hash['dependencies']
-      @name = hash['name']
-      @productName = hash['productName']
-      @productReference = hash['productReference']
-      @productType = hash['productType']
+      Fields.each do |attr|
+        self.class.send(:attr_accessor,attr)
+        instance_variable_set('@'+attr,hash[attr])
+      end
     end
 
 
--- Components/PBXClasses/pbx_file_reference.rb
+++ Components/PBXClasses/pbx_file_reference.rb
@@ -3,7 +3,7 @@ require_relative '../abstract_object'
 module AppContainer
 
   class PBXFileReference < AbstractObject
-    ::Fields = ['explicitFileType',
+    Fields = ['explicitFileType',
                 'lastKnownFileType',
                 'path',
                 'sourceTree',
@@ -12,7 +12,7 @@ module AppContainer
 
     def initialize(hash={})
       super(self)
-      ::Fields.each do |attr|
+      Fields.each do |attr|
         self.class.send(:attr_accessor,attr)
         instance_variable_set('@'+attr,hash[attr])
       end
